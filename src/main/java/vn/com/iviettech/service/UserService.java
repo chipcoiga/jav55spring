e711a10cd19d8e9255f57a5049b555ec01b630f1
vantruong1512 <truongnd0199@gmail.com>
2025-06-24 18:09:58 +0700

    Bai tap 1

--- src/main/java/vn/com/iviettech/service/UserService.java
+++ src/main/java/vn/com/iviettech/service/UserService.java
@@ -3,7 +3,10 @@ package vn.com.iviettech.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import vn.com.iviettech.domain.Account;
 import vn.com.iviettech.repository.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -18,5 +21,19 @@ public class UserService {
         System.out.println("Print form service");
         repository.printSomething();
         /// business logic / xử lý tính năng theo requirement
+    }
+
+    public void addAccount(){
+
+    }
+
+    public void searchAccount(){
+        System.out.println("print from service");
+        List<Account> accounts = repository.getAccount();
+        for(Account account:accounts){
+            if(account.getName().contains("ng")){
+                System.out.println(account.getName());
+            }
+        }
     }
 }
