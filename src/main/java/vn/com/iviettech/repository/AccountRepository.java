7a53a0657e957b5f186de251f5b91a859a5cfb5d
vandat380 <dat0708063962@gmail.com>
2025-06-26 20:03:46 +0700

    nộp bài tập

---
+++ src/main/java/vn/com/iviettech/repository/AccountRepository.java
@@ -0,0 +1,16 @@
+    package vn.com.iviettech.repository;
+
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.stereotype.Repository;
+    import vn.com.iviettech.entity.AccountEntity;
+
+    import java.util.List;
+
+    @Repository
+    public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+
+        List<AccountEntity> findAllByNameContainsAndSalaryGreaterThan(String name, Long salary);
+
+        List<AccountEntity> findBySalaryBetween(Long min, Long max);
+
+    }
---
+++ src/main/java/vn/com/iviettech/service/AccountService.java
@@ -0,0 +1,65 @@
+package vn.com.iviettech.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+import vn.com.iviettech.entity.AccountEntity;
+import vn.com.iviettech.repository.AccountRepository;
+
+@Service
+public class AccountService {
+
+    private AccountRepository repository;
+
+    public AccountService(AccountRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createAccount(AccountEntity entity) {
+        repository.save(entity);
+    }
+
+    @PostConstruct
+    public void initData() {
+        repository.findAllByNameContainsAndSalaryGreaterThan("", 10000000L)
+                .forEach(entity -> {
+                    System.out.println(entity.getId());
+                    System.out.println(entity.getName());
+                    System.out.println(entity.getSalary());
+                    System.out.println("======");
+                });
+
+//        repository.findAllByNameContains("4").forEach(entity -> {
+//            System.out.println(entity.getId());
+//            System.out.println(entity.getName());
+//            System.out.println(entity.getSalary());
+//            System.out.println("======");
+//        });
+
+//        repository.findAll().forEach(entity -> {
+//            System.out.println(entity.getId());
+//            System.out.println(entity.getName());
+//            System.out.println(entity.getSalary());
+//            System.out.println("======");
+//        });
+
+//        AccountEntity entity1 = new AccountEntity();
+//        entity1.setName("Account 1");
+//        entity1.setSalary(10000000L);
+//        createAccount(entity1);
+//
+//        AccountEntity entity2 = new AccountEntity();
+//        entity2.setName("Account 2");
+//        entity2.setSalary(15000000L);
+//        createAccount(entity2);
+//
+//        AccountEntity entity3 = new AccountEntity();
+//        entity3.setName("Account 3");
+//        entity3.setSalary(5000000L);
+//        createAccount(entity3);
+//
+//        AccountEntity entity4 = new AccountEntity();
+//        entity4.setName("Account 4");
+//        entity4.setSalary(50000000L);
+//        createAccount(entity4);
+    }
+}
