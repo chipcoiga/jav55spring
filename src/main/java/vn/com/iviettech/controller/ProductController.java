61bbdf28ee6f047578a2e4655b564827aa4b9d70
lehuuloc <lehuuloc.qt@gmail.com>
2025-07-03 20:32:38 +0700

    demo web

---
+++ src/main/java/vn/com/iviettech/controller/ProductController.java
@@ -0,0 +1,9 @@
+package vn.com.iviettech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("products")
+public class ProductController {
+}
