f31b5c331eae7b5451e6154ec58eb05f135998b5
Khang <vinhkhang1506@gmailcom>
2025-07-15 16:46:36 +0700

    bai tap 9

--- src/main/java/vn/com/iviettech/controller/UserController.java
+++ src/main/java/vn/com/iviettech/controller/UserController.java
@@ -1,29 +1,126 @@
 package vn.com.iviettech.controller;
 
+import jakarta.validation.Valid;
+import org.apache.catalina.Group;
+import org.apache.catalina.Role;
+import org.apache.catalina.User;
+import org.apache.catalina.UserDatabase;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import vn.com.iviettech.domain.User;
 
-@RequestMapping("users")
+import java.util.Iterator;
+
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("register")
-    public String showUserRegisterForm(Model model) {
-        model.addAttribute("user", new User());
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User() {
+            @Override
+            public String getFullName() {
+                return "";
+            }
 
-        return "user-register";
+            @Override
+            public void setFullName(String s) {
+
+            }
+
+            @Override
+            public Iterator<Group> getGroups() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return "";
+            }
+
+            @Override
+            public void setPassword(String s) {
+
+            }
+
+            @Override
+            public Iterator<Role> getRoles() {
+                return null;
+            }
+
+            @Override
+            public UserDatabase getUserDatabase() {
+                return null;
+            }
+
+            @Override
+            public String getUsername() {
+                return "";
+            }
+
+            @Override
+            public void setUsername(String s) {
+
+            }
+
+            @Override
+            public void addGroup(Group group) {
+
+            }
+
+            @Override
+            public void addRole(Role role) {
+
+            }
+
+            @Override
+            public boolean isInGroup(Group group) {
+                return false;
+            }
+
+            @Override
+            public boolean isInRole(Role role) {
+                return false;
+            }
+
+            @Override
+            public void removeGroup(Group group) {
+
+            }
+
+            @Override
+            public void removeGroups() {
+
+            }
+
+            @Override
+            public void removeRole(Role role) {
+
+            }
+
+            @Override
+            public void removeRoles() {
+
+            }
+
+            @Override
+            public String getName() {
+                return "";
+            }
+        });
+        return "register";  // Tên file JSP
     }
 
-    @PostMapping("register")
-    public String regisUser(User user) {
-
-        //Call Service save to database
-        //userService.createUser(user);
-
-        return "user-information";
+    @PostMapping("/register")
+    public String processForm(@Valid @ModelAttribute("user") User user,
+                              BindingResult result) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        return "userProfile";  // Trang thành công
     }
 }
